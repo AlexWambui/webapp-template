f9ac75ca44923f0f307405d65d5f1ec47b3b3dc2
AlexAaqil <alexaaqil.se@gmail.com>
2026-09-02 17:35:13 +0300

    added the support service provider.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,4 +6,6 @@ use App\Providers\FortifyServiceProvider;
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
+
+    \Modules\Support\Providers\SupportServiceProvider::class,
 ];
